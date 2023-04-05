33698e17e57927713d91f2924a2ed3abf49b0b8a
Stas Yakobov <stas@3os.org>
2023-04-06 01:38:54 +0300

    20230406.0138

---
+++ git-push.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Ceack if inotifywait is installed if not install it
+if ! [ -x "$(command -v inotifywait)" ]; then
+  echo 'Error: inotifywait is not installed.' >&2
+  echo 'Installing inotifywait'
+  sudo apt install -y inotify-tools
+fi
+
+path="/home/pi/printer_data/config"
+echo "Watching $path"
+
+# watch for changes in the path
+while inotifywait -e modify -e create -e delete -e move -e attrib $path; do
+  echo "Change detected in $path"
+  echo "Pushing to git"
+  # cd to path
+  /usr/bin/git --git-dir=$path/.git --work-tree=$path add $path
+  # commit with date
+  /usr/bin/git --git-dir=$path/.git --work-tree=$path commit -m $(date +'%Y%m%d.%H%M')
+  # push to git repo
+  /usr/bin/git --git-dir=$path/.git --work-tree=$path push
+  echo "Pushed to git"
+done
